67f45357975df23caedd2d780985114eea327a8f
AbdulMalikMahmud <mahmudmalik1222@gmail.com>
2023-11-12 00:05:42 +0000

    created a product.js file

---
+++ product.js
@@ -0,0 +1,12 @@
+const products = [
+    { id: 1, name: 'Bag', price: 15, location: 'Brentwood', quantity: 5, image: "images/bag.png" },
+    { id: 2, name: 'Charger', price: 5, location: 'Hendon', quantity: 5, image: "images/charger.png" },
+    { id: 3, name: 'Dictionary', price: 7, location: 'Brentcross', quantity: 5, image: "images/dictionary.png" },
+    { id: 4, name: 'Stapler', price: 40, location: 'Liverpool', quantity: 5, image: "images/stapler.png" },
+    { id: 5, name: 'Eraser', price: 6, location: 'Edgware', quantity: 5, image: "images/eraser.png" },
+    { id: 6, name: 'Idcard', price: 10, location: 'Camden', quantity: 5, image: "images/idcard.png" },
+    { id: 7, name: 'Laptop', price: 500, location: 'Euston', quantity: 5, image: "images/laptop.png" },
+    { id: 8, name: 'Modem', price: 120, location: 'Moorgate', quantity: 5, image: "images/modem.png" },
+    { id: 9, name: 'Monitor', price: 200, location: 'Stratford', quantity: 5, image: "images/monitor.png" },
+    { id: 10, name: 'Pencil', price: 1, location: 'Whitechapel', quantity: 5, image: "images/pemcil.png" },
+]
